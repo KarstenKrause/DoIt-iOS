5f05ca89f1f4bbdad57dd5135316900c11a83044
Karsten Krause <karsten.krause1988@gmail.com>
2023-06-18 21:59:51 +0200

    refactoring

---
+++ DoIt-iOS/Components/PrimaryButton.swift
@@ -0,0 +1,20 @@
+//
+//  PrimaryButton.swift
+//  DoIt-iOS
+//
+//  Created by Karsten Krause on 15.06.23.
+//
+
+import SwiftUI
+
+struct PrimaryButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PrimaryButton_Previews: PreviewProvider {
+    static var previews: some View {
+        PrimaryButton()
+    }
+}
